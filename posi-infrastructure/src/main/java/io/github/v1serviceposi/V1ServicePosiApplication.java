edd5576625bc523155578943645c9560d35f36a1
J2KB-admin <sunget.co12@gmail.com>
2022-08-04 06:28:54 +0000

    Rename Files

--- posi-infrastructure/src/main/java/io/github/v1serviceposi/V1ServicePosiApplication.java
+++ posi-infrastructure/src/main/java/io/github/v1serviceposi/V1ServicePosiApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class V1ServiceApplication {
+public class V1ServicePosiApplication {
     public static void main(String[] args) {
-        SpringApplication.run(V1ServiceApplication.class, args);
+        SpringApplication.run(V1ServicePosiApplication.class, args);
     }
 }
